f5cfdb43cc26b4322cafae339c6950c389a12c4a
Kieran Kohtz <kohtzk@gmail.com>
2020-12-04 15:51:31 +0000

    added controls

--- frontend/marking.js
+++ frontend/marking.js
@@ -2,6 +2,34 @@ $.post("/answers", {"secret": "FuckYourChickenStrips"}, function(result) {
     console.log(result);
 });
 
-// function test(result) {
-//     Console.log(result);
-// }
+$(document).ready(function () {
+    $("#prev").click(function () {
+        console.log("prev");
+        $.post("/control", {"secret": "FuckYourChickenStrips", "command": "prev"}, function(result) {
+            console.log(result);
+        });
+    });
+
+
+    $("#next").click(function () {
+        console.log("next");
+        $.post("/control", {"secret": "FuckYourChickenStrips", "command": "next"}, function(result) {
+            console.log(result);
+        });
+    });
+
+
+    $("#open").click(function () {
+        console.log("open");
+        $.post("/control", {"secret": "FuckYourChickenStrips", "command": "open"}, function(result) {
+            console.log(result);
+        });
+    });
+
+    $("#close").click(function () {
+        console.log("close");
+        $.post("/control", {"secret": "FuckYourChickenStrips", "command": "close"}, function(result) {
+            console.log(result);
+        });
+    });
+});
